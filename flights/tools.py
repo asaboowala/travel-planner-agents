df1ea73e95cd528d9ad1d462e39e99b5db02b252
asaboowala <alisha3@gmail.com>
2025-09-20 12:34:32 -0700

    use env for file path to flights MCP server

--- flights/tools.py
+++ flights/tools.py
@@ -1,11 +1,12 @@
 import os
 from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset
 from google.adk.tools.mcp_tool.mcp_session_manager import StdioConnectionParams, StdioServerParameters
+from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv())
 
 # --- Flights MCP (Python stdio via uv) ---
-# Update this path to your local clone
-FLIGHTS_DIR = os.path.expanduser("/Users/alishasaboowala/Desktop/GenAI_Projects/Medium/google-adk/flights-mcp")
+FLIGHTS_DIR = os.path.expanduser(os.getenv("FLIGHTS_DIR"))
 flights_toolset = MCPToolset(
     connection_params=StdioConnectionParams(
         server_params=StdioServerParameters(
